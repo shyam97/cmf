0fb410615f17963aa06ed324d3b7b38a36b75a90
Shyam Sundar Hemamalini <shyam.hemamalini@gmail.com>
2020-04-06 12:10:35 +0200

    Updated democode

--- democode.py
+++ democode.py
@@ -1,5 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+start = time.time()
 
 print("Hello")
 
 print(np.pi)
+
+plt.scatter(0,0)
+plt.show()
+
+print(time.time() - start)
