024cb2e3f46e896738cff576a9be8163b5c4e01e
Shyam Sundar Hemamalini <shyam.hemamalini@gmail.com>
2020-05-17 12:03:54 +0200

    some edits

--- fluid side/mesh.py
+++ fluid side/mesh.py
@@ -55,7 +55,7 @@ def cellcenter(grid):
 
     return pgrid
 
-def create_mesh(l,w,h_x,h_y):
+def create_mesh(l,w,h_x,h_y,viewgrid):
 
     L = l+h_x
     W = w+h_y
@@ -72,32 +72,33 @@ def create_mesh(l,w,h_x,h_y):
     # print(grid)
     # print(pgrid)
 
-    for i in range(n_x+2):
-        for j in range(n_y+2):
-            plt.figure(num=1)
-            plt.scatter(grid[0,i,j],grid[1,i,j],c='k',s=10)
+    if viewgrid==1:
+        for i in range(n_x+2):
+            for j in range(n_y+2):
+                plt.figure(num=1)
+                plt.scatter(grid[0,i,j],grid[1,i,j],c='k',s=10)
 
-            if i<n_x+1 and j<n_y+1:
-                plt.scatter(pgrid[0,i,j],pgrid[1,i,j],c='r',s=10)
+                if i<n_x+1 and j<n_y+1:
+                    plt.scatter(pgrid[0,i,j],pgrid[1,i,j],c='r',s=10)
 
-            if i<n_x+1 and j<n_y:
-                plt.scatter(vgrid[0,i,j],vgrid[1,i,j],c='b',s=10,marker='x')
+                if i<n_x+1 and j<n_y:
+                    plt.scatter(vgrid[0,i,j],vgrid[1,i,j],c='b',s=10,marker='x')
 
-            if j<n_y+1 and i<n_x:
-                plt.scatter(ugrid[0,i,j],ugrid[1,i,j],c='g',s=10,marker='x')
+                if j<n_y+1 and i<n_x:
+                    plt.scatter(ugrid[0,i,j],ugrid[1,i,j],c='g',s=10,marker='x')
 
-    plt.plot([0,l],[0,0],'k')
-    plt.plot([l,l],[0,w],'k')
-    plt.plot([l,0],[w,w],'k')
-    plt.plot([0,0],[w,0],'k')
-    plt.show()
-
-    plt.figure()
-    plt.plot([0,l],[0,0],'k')
-    plt.plot([l,l],[0,w],'k')
-    plt.plot([l,0],[w,w],'k')
-    plt.plot([0,0],[w,0],'k')
-    plt.scatter(ugrid[0,0,1],ugrid[1,0,1])
-    plt.show()
+        plt.plot([0,l],[0,0],'k')
+        plt.plot([l,l],[0,w],'k')
+        plt.plot([l,0],[w,w],'k')
+        plt.plot([0,0],[w,0],'k')
+        plt.show()
 
     return grid,pgrid,ugrid,vgrid
+
+def matrix(grid):
+    n_x = len(grid[0])-1
+    n_y = len(grid[0][0])-1
+
+    m = np.zeros((n_x,n_y))
+
+    return m
--- fluid side/solver.py
+++ fluid side/solver.py
@@ -6,5 +6,20 @@ l = 2
 w = 1
 h_x = 0.1
 h_y = 0.1
+md = 0
 
-grid,pgrid,ugrid,vgrid = create_mesh(l,w,h_x,h_y)
+rho_s = 1.25
+p_a = 101325
+
+grid,pgrid,ugrid,vgrid = create_mesh(l,w,h_x,h_y,md)
+
+p = matrix(pgrid) + p_a
+u = matrix(ugrid)
+v = matrix(vgrid)
+rho = matrix(pgrid) + rho_s
+visc = matrix(pgrid)
+
+plt.figure()
+plt.imshow(p.T, origin = 'lower', extent=[0,l,0,w])
+plt.colorbar()
+plt.show()
