4e7f49b0f19fae775c9e640a178bdb8da8a120c7
Shyam Sundar Hemamalini <shyam.hemamalini@gmail.com>
2020-05-17 14:20:01 +0200

    Update solver.py

--- fluid side/solver.py
+++ fluid side/solver.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mesh import *
 
-l = 2
-w = 1
-h_x = 0.1
-h_y = 0.1
+l = 0.2
+w = 0.1
+h_x = 0.01
+h_y = 0.001
 md = 0
 
 rho_s = 1.25
@@ -19,7 +19,10 @@ v = matrix(vgrid)
 rho = matrix(pgrid) + rho_s
 visc = matrix(pgrid)
 
-plt.figure()
-plt.imshow(p.T, origin = 'lower', extent=[0,l,0,w])
-plt.colorbar()
-plt.show()
+v[:,0] = 0
+v[:,-1] = 0
+u[0,:] = 1
+p[:,0] = 0
+p[:,-1] = 0
+rho[:,0] = 1e10
+rho[:,-1] = 1e10
