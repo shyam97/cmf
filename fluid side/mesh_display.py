c8f28e52d8a096c9d3645d365fa57b3a206ea1c6
raviramesh10 <raviramesh26@gmail.com>
2020-05-17 09:38:40 +0200

    update mesh_sgrid

--- fluid side/mesh_display.py
+++ fluid side/mesh_display.py
@@ -12,13 +12,26 @@ from cellcenter import *
 from init_mesh import *
 from facecenter import *
 
-L = 10
-W = 5
-h_x = 0.1
-h_y = 0.05
+#Mesh parameters
+L = 1e-2
+W = 5e-3
+h_x = 1e-5
+h_y = 1e-5
+dt=0.0004
+nstep=100
+maxiter=1000
+maxError=0.001
+beta=1.2
 
 n_x = int(L/h_x)
 n_y = int(W/h_y)
+r=np.ones((nx+2,ny+2))*rho1 #Residual matrix for density, not necessary if incompressible
+
+#tangential velocities
+unorth=2.
+usouth=0.
+veast=0.
+vwest=0.
 
 grid = init_mesh(L,W,h_x,h_y)
 pgrid = cellcenter(grid)
@@ -28,6 +41,8 @@ vgrid = facecenter(grid,1)
 # print(grid)
 # print(pgrid)
 
+
+#Displaying mesh
 for i in range(n_x+1):
     for j in range(n_y+1):
         plt.figure(num=1)
@@ -41,5 +56,59 @@ for i in range(n_x+1):
 
         if j<n_y:
             plt.scatter(ugrid[0,i,j],ugrid[1,i,j],c='g',s=10,marker='x')
+
+
+#Defining velocities and pressures
+
+u=np.zeros((n_x+1,n_y+2))
+v=np.zeros((n_x+2,n_y+1))
+p=np.zeros((n_x+2,n_y+2))
+
+ut=np.zeros((n_x+1,n_y+2))
+vt=np.zeros((n_x+2,n_y+1))
+
+#temp variables for pressure calculations
+tmp1=np.zeros((n_x+2,n_y+2))
+tmp2=np.zeros((n_x+2,n_y+2))
+
+#velocities at center of grid for plotting
+uu=np.zeros((n_x+1,n_y+1))
+vv=np.zeros((n_x+1,n_y+1))
+x = np.linspace(0.,Lx,n_x)
+y = np.linspace(0.,Ly,n_y)
+
+
+time=0.
+for steps in range(nstep):
+    
+    #tangential velocity at boundaries
+    u[:,0]=2.*usouth-u[:,1]
+    u[:,-1]=2.*unorth-u[:,-2]
+    v[0,:]=2.*vwest-v[1,:]
+    v[-1,:]=2.*veast-v[-2,:]
+
+    
+    # TEMPORARY u-velocity                               
+    for i in range(1,nx):
+        for j in range(1,ny+1):
+            ut[i,j]=u[i,j]+dt*(-0.25*(((u[i+1,j]+u[i,j])**2-(u[i,j]+\
+                u[i-1,j])**2)/dx+((u[i,j+1]+u[i,j])*(v[i+1,j]+\
+                v[i,j])-(u[i,j]+u[i,j-1])*(v[i+1,j-1]+v[i,j-1]))/dy)+\
+                m0/(0.5*(r[i+1,j]+r[i,j]))*((u[i+1,j]-2*u[i,j]+u[i-1,j])/dx**2+\
+                (u[i,j+1]-2*u[i,j]+u[i,j-1])/dy**2 )+gx)
+
+    # TEMPORARY v-velocity                               
+    for i in range(1,nx+1):
+        for j in range(1,ny):
+            vt[i,j]=v[i,j]+dt*(-0.25*(((u[i,j+1]+u[i,j])*(v[i+1,j]+\
+                v[i,j])-(u[i-1,j+1]+u[i-1,j])*(v[i,j]+v[i-1,j]))/dx+\
+                ((v[i,j+1]+v[i,j])**2-(v[i,j]+v[i,j-1])**2)/dy)+\
+                m0/(0.5*(r[i,j+1]+r[i,j]))*((v[i+1,j]-2*v[i,j]+v[i-1,j])/dx**2+\
+                (v[i,j+1]-2*v[i,j]+v[i,j-1])/dy**2 )+gy)
+
+
+
+
+
 
 plt.show()
---
+++ fluid side/mesh_sgrid.py
@@ -0,0 +1,143 @@
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython import display
+#matplotlib inline
+
+#domain size and physical variables
+Lx=10.0
+Ly=5.0
+gx=0.0
+gy=0.0
+rho1=1.0
+
+#dynamic viscosity
+m0=0.05
+
+#tangential velocities
+unorth=2.
+usouth=0.
+veast=0.
+vwest=0.
+
+#Numerical variables
+nx=21
+ny=21
+dt=0.0004
+nstep=100
+maxiter=1000
+maxError=0.001
+beta=1.2
+
+u=np.zeros((nx+1,ny+2))
+v=np.zeros((nx+2,ny+1))
+p=np.zeros((nx+2,ny+2))
+
+ut=np.zeros((nx+1,ny+2))
+vt=np.zeros((nx+2,ny+1))
+
+#temp variables for pressure calculations
+tmp1=np.zeros((nx+2,ny+2))
+tmp2=np.zeros((nx+2,ny+2))
+
+#velocities at center of grid for plotting
+uu=np.zeros((nx+1,ny+1))
+vv=np.zeros((nx+1,ny+1))
+x = np.linspace(0.,Lx,nx)
+y = np.linspace(0.,Ly,ny)
+
+#Define the grid
+dx=Lx/(nx) 
+dy=Ly/(ny)
+
+
+r=np.ones((nx+2,ny+2))*rho1
+            
+time=0.
+for steps in range(nstep):
+    
+    #tangential velocity at boundaries
+    u[:,0]=2.*usouth-u[:,1]
+    u[:,-1]=2.*unorth-u[:,-2]
+    v[0,:]=2.*vwest-v[1,:]
+    v[-1,:]=2.*veast-v[-2,:]
+    
+    #pressure boundary condition to be imposed
+
+    # TEMPORARY u-velocity                               
+    for i in range(1,nx):
+        for j in range(1,ny+1):
+            ut[i,j]=u[i,j]+dt*(-0.25*(((u[i+1,j]+u[i,j])**2-(u[i,j]+\
+                u[i-1,j])**2)/dx+((u[i,j+1]+u[i,j])*(v[i+1,j]+\
+                v[i,j])-(u[i,j]+u[i,j-1])*(v[i+1,j-1]+v[i,j-1]))/dy)+\
+                m0/(0.5*(r[i+1,j]+r[i,j]))*((u[i+1,j]-2*u[i,j]+u[i-1,j])/dx**2+\
+                (u[i,j+1]-2*u[i,j]+u[i,j-1])/dy**2 )+gx)
+
+    # TEMPORARY v-velocity                               
+    for i in range(1,nx+1):
+        for j in range(1,ny):
+            vt[i,j]=v[i,j]+dt*(-0.25*(((u[i,j+1]+u[i,j])*(v[i+1,j]+\
+                v[i,j])-(u[i-1,j+1]+u[i-1,j])*(v[i,j]+v[i-1,j]))/dx+\
+                ((v[i,j+1]+v[i,j])**2-(v[i,j]+v[i,j-1])**2)/dy)+\
+                m0/(0.5*(r[i,j+1]+r[i,j]))*((v[i+1,j]-2*v[i,j]+v[i-1,j])/dx**2+\
+                (v[i,j+1]-2*v[i,j]+v[i,j-1])/dy**2 )+gy)    
+    
+    #Compute source term and the coefficient for p(i,j)
+    rt=r.copy()
+    lrg=1000.
+    rt[:,0]=lrg
+    rt[:,-1]=lrg
+    rt[0,:]=lrg
+    rt[-1,:]=lrg
+    
+    for i in range(1,nx+1):
+        for j in range(1,ny+1):
+            tmp1[i,j]= (0.5/dt)*( (ut[i,j]-ut[i-1,j])/dx+(vt[i,j]-vt[i,j-1])/dy)
+            tmp2[i,j]=1.0/( (1./dx)*( 1./(dx*(rt[i+1,j]+rt[i,j]))+\
+                1./(dx*(rt[i-1,j]+rt[i,j])))+\
+                (1./dy)*(1./(dy*(rt[i,j+1]+rt[i,j]))+\
+                1./(dy*(rt[i,j-1]+rt[i,j])))) 
+
+    iter=0
+    while True:
+        pn=p.copy()
+        iter=iter+1
+        for i in range(1,nx+1):
+            for j in range(1,ny+1):
+                p[i,j]=(1.0-beta)*p[i,j]+beta*tmp2[i,j]*(\
+                    (1./dx)*( p[i+1,j]/(dx*(rt[i+1,j]+rt[i,j]))+\
+                    p[i-1,j]/(dx*(rt[i-1,j]+rt[i,j])))+\
+                    (1./dy)*( p[i,j+1]/(dy*(rt[i,j+1]+rt[i,j]))+\
+                    p[i,j-1]/(dy*(rt[i,j-1]+rt[i,j])))-tmp1[i,j])
+        if np.abs(pn-p).max()<maxError:
+            break
+        if iter>maxiter:
+            break
+    
+    #CORRECT THE u-velocity
+    for i in range(1,nx):
+        for j in range(1,ny+1):
+            u[i,j]=ut[i,j]-dt*(2.0/dx)*(p[i+1,j]-p[i,j])/(r[i+1,j]+r[i,j])
+    
+    #CORRECT THE v-velocity
+    for i in range(1,nx+1):
+        for j in range(1,ny):
+            v[i,j]=vt[i,j]-dt*(2.0/dy)*(p[i,j+1]-p[i,j])/(r[i,j+1]+r[i,j])
+
+    time=time+dt
+    
+    #Plot the velocity field
+    uu=0.5*(u[0:nx,1:ny+1]+u[0:nx,0:ny])
+    vv=0.5*(v[1:nx+1,0:ny]+v[0:nx,0:ny])
+    yy,xx=np.mgrid[0:(nx-1)*dx:nx*1j,0:(ny-1)*dx:ny*1j]
+    plt.clf()
+    plt.quiver(xx,yy,uu.T,vv.T)
+    # display.clear_output(wait=True)
+    # display.display(plt.gcf())
+    print(steps)
+
+plt.figure()
+plt.clf()
+plt.streamplot(x,y,uu.T,vv.T)
+
+plt.show()
