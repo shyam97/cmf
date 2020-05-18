63f51ffe3d789def25dd2adbe593b6ebcc6eca63
Shyam Sundar Hemamalini <shyam.hemamalini@gmail.com>
2020-05-18 22:44:18 +0200

    v2

--- fluid side/solver.py
+++ fluid side/solver.py
@@ -4,38 +4,154 @@ from mesh import *
 
 l = 0.2
 w = 0.1
-h_x = 0.01
-h_y = 0.01
+nx = 20
+ny = 20
 md = 0
+h_x = l/(nx)
+h_y = w/(ny)
 
-rho_s = 1.25
+rho_s = 1
 p_a = 101325
-nu_s = 1.48e-5
+mu_s = 0.05
 
-dt = 1e-2
-tsteps = 1e3
+dt = 1e-4
+tsteps = 1
+beta = 0.8 #Over-relaxation parameter
+maxiter = 1000
+maxError = 1e-3
 
 grid,pgrid,ugrid,vgrid = create_mesh(l,w,h_x,h_y,md)
 
 p = matrix(pgrid) + p_a
-u = matrix(ugrid)
+p1 = p
+p2 = p
+u = matrix(ugrid) + 0.5
 ustar = matrix(ugrid)
 vstar = matrix(vgrid)
 v = matrix(vgrid)
 rho = matrix(pgrid) + rho_s
-nu = matrix(pgrid) + nu_s
+mu = matrix(pgrid) + mu_s
+gx = 0
+gy = 0
 
-v[:,0] = 0
-v[:,-1] = 0
-u[0,:] = 1
-p[:,0] = 0
-p[:,-1] = 0
-rho[:,0] = 1e10 #
-rho[:,-1] = 1e10
+print(nx)
+print(ny)
+print(p.shape)
+print(u.shape)
+print(v.shape)
 
-for t in range(1,tsteps):
+v[:,0] = 0 #Vertical component of velocity at bottom wall
+v[:,-1] = 0 #Vertical component of velocity at top wall
+u[0,:] = 1 #Horizontal component of velocity defined at the inlet, to be changed to another profile later
+p[:,0] = 0 #Pressure at the bottom wall ghost cells
+p[:,-1] = 0 #Pressure at the top wall ghost cells
+# rho[:,0] = 1e10 #Density at the bottom wall ghost cells
+# rho[:,-1] = 1e10 #Density at the top wall ghost cells
+time = 0
+for t in range(0,tsteps):
     time = time + dt
+    print(time)
 
-    u[:,0] = -u[:,1]
-    u[:,-1] = -u[:,-2]
-    p[-1,:] = 2*p_a - p[-2,:]
+    u[:,0] = -u[:,1] #Horizontal component of velocity at bottom wall ghost
+    u[:,-1] = -u[:,-2] #Horizontal component of velocity at top wall ghost
+    v[0,:] = -v[1,:] #Vertical component of velocity at ghost cell at inlet
+    v[-1,:] = v[-2,:] #Vertical component of velocity at ghost cell at outlet
+    p[-1,:] = 2*p_a - p[-2,:]  #Pressure at the ghost cells at the outlet
+    p[0,:] = 2*p_a #Pressure at the inlet (to be changed later)
+
+    for i in range(1,len(u)-1):
+        for j in range(1,len(u[0])-1):
+
+            #Advection terms - x component
+            Ax = ((u[i+1,j] + u[i,j])**2 - (u[i,j]+u[i-1,j])**2)/(4*h_x) +\
+                 ((u[i,j+1] + u[i,j])*(v[i+1,j] + v[i,j]) - (u[i,j] + u[i,j-1])*\
+                   (v[i+1,j-1] + v[i,j-1]))/(4*h_y)
+
+            #Diffusion terms - x component
+            Dx = 0.5*(mu[i+1,j] + mu[i,j])*\
+                ((u[i+1,j] -2*u[i,j] + u[i-1,j])/(h_x**2) +\
+                 (u[i,j+1] -2*u[i,j] + u[i,j-1])/(h_y**2))
+
+            #Predictor step - x component
+            ustar[i,j] = u[i,j] + dt*(-Ax + (2/(rho[i,j] + rho[i+1,j]))*Dx + gx)
+
+    for i in range(1,len(v)-1):
+        for j in range(1,len(v[0])-1):
+
+            #Advection terms - y component
+            Ay = ((u[i,j+1] + u[i,j])*(v[i,j] + v[i+1,j]) - (u[i-1,j+1] + u[i-1,j])*\
+                  (v[i,j] + v[i-1,j]))/(4*h_x) + ((v[i,j+1] + v[i,j])**2 -\
+                  (v[i,j] + v[i,j-1])**2)/(4*h_y)
+
+            #Diffusion terms - y component
+            Dy = 0.5*(mu[i,j+1] + mu[i,j])*\
+                ((v[i+1,j] - 2*v[i,j] + v[i-1,j])/(h_x**2) +\
+                 (v[i,j+1] - 2*v[i,j] + v[i,j-1])/(h_y**2))
+
+            #Predictor step - y component
+            vstar[i,j] = v[i,j] + dt*(-Ay + gy + 2*Dy/((rho[i,j+1] + rho[i,j])))
+
+    r = rho.copy()
+    r[:,0] = 1e3
+    r[:,-1] = 1e3
+
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    plt.figure()
+    ax = plt.gca()
+    im = ax.imshow(ustar.T, origin = 'lower', extent=[-h_x,l+h_x,-h_y,w+h_y])
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im, cax=cax)
+    plt.show()
+
+#Defining the constants in the corrector step
+    for i in range(1,len(p)-1):
+        for j in range(1,len(p[0])-1):
+            #Constant on the LHS
+            p1[i,j]= (0.5/dt)*((ustar[i,j]-ustar[i-1,j])/h_x +\
+                                (vstar[i,j]-vstar[i,j-1])/h_y)
+            #Constant on the RHS
+            p2[i,j]=1.0/((1./h_x)*(1./(h_x*(r[i+1,j]+r[i,j]))+\
+                1./(h_x*(r[i-1,j]+r[i,j])))+\
+                (1./h_y)*(1./(h_y*(r[i,j+1]+r[i,j]))+\
+                1./(h_y*(r[i,j-1]+r[i,j]))))
+
+    #Pressure equation - to be solved implicitly
+
+    iter=0
+    while True:
+        pn=p.copy()
+        iter=iter+1
+        for i in range(1,len(p)-1):
+            for j in range(1,len(p[0])-1):
+                p[i,j]=(1.0-beta)*p[i,j]+beta*p2[i,j]*(\
+                    (1./h_x)*( p[i+1,j]/(h_x*(r[i+1,j]+r[i,j]))+\
+                    p[i-1,j]/(h_x*(r[i-1,j]+r[i,j])))+\
+                    (1./h_y)*( p[i,j+1]/(h_y*(r[i,j+1]+r[i,j]))+\
+                    p[i,j-1]/(h_y*(r[i,j-1]+r[i,j])))- p1[i,j])
+        print(np.abs(pn-p).max())
+        if np.abs(pn-p).max()<maxError:
+            break
+        if iter>maxiter:
+            break
+
+    #Corrector step - to be done implicitly
+
+    #CORRECT THE u-velocity
+    for i in range(1,len(u)-1):
+        for j in range(1,len(u[0])-1):
+            u[i,j]=ustar[i,j]-dt*(2.0/h_x)*(p[i+1,j]-p[i,j])/(rho[i+1,j]+rho[i,j])
+
+    #CORRECT THE v-velocity
+    for i in range(1,len(v)-1):
+        for j in range(1,len(v[0])-1):
+            v[i,j]=vstar[i,j]-dt*(2.0/h_y)*(p[i,j+1]-p[i,j])/(rho[i,j+1]+rho[i,j])
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+plt.figure()
+ax = plt.gca()
+im = ax.imshow(u.T, origin = 'lower', extent=[-h_x,l+h_x,-h_y,w+h_y])
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+plt.colorbar(im, cax=cax)
+plt.show()
---
+++ fluid side/v2.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# GRID SETTINGS
+l = 10
+w = 5
+nx = 100
+ny = 50
+h_x = l/nx
+h_y = w/ny
+
+# TIME STEP SETTINGS
+dt = 1e-4
+tsteps = 3
+time = 0
+
+# ITERATION SETTINGS
+maxiter = 100
+maxerror = 1e-3
+beta = 1.0 #Over-relaxation parameter
+
+# STATIC PARAMETERS
+rho_s = 1000
+rho_w = 1e10
+mu_s = 1e-5
+p_s = 1e5
+gx = 0
+gy = 0
+
+# INITIALIZING MATRICES, defined using length of the interval and not number of grid points
+u = np.zeros((nx+1,ny+2))
+v = np.zeros((nx+2,ny+1))
+p = np.zeros((nx+2,ny+2))
+
+# INTERMEDIATE VELOCITIES
+ustar = u.copy()
+vstar = v.copy()
+
+# COEFFICIENTS FOR PRESSURE
+p1 = p.copy()
+p2 = p.copy()
+
+# DENSITY AND VISCOSITY MATRIX
+rho = np.ones((nx+2,ny+2))*rho_s
+mu = np.ones((nx+2,ny+2))*mu_s
+
+# STATIC BOUNDARIES
+u[0,:] = 1
+
+# GRIDS FOR PLOTTING
+uu=np.zeros((nx+1,ny+1))
+vv=np.zeros((nx+1,ny+1))
+uu=0.5*(u[0:nx,1:ny+1]+u[0:nx,0:ny])
+vv=0.5*(v[1:nx+1,0:ny]+v[0:nx,0:ny])
+x = np.linspace(0.,l,nx)
+y = np.linspace(0.,w,ny)
+
+for t in range(0,tsteps):
+    time = time + dt
+    print(time)
+
+    # TANGENTIAL VELOCITY AT BOUNDARIES
+    u[:,0] = -u[:,1]
+    u[:,-1] = -u[:,-2]
+
+    v[0,:] = -v[1,:]
+    v[-1,:] = v[-2,:]
+
+    p[-1,:] = 2*p_s - p[-2,:]
+
+    # U STAR
+    for i in range(1,nx):
+        for j in range(1,ny+1):
+            ustar[i,j]=u[i,j]+dt*(-0.25*(((u[i+1,j]+u[i,j])**2-(u[i,j]+\
+                u[i-1,j])**2)/h_x +((u[i,j+1]+u[i,j])*(v[i+1,j]+\
+                v[i,j])-(u[i,j]+u[i,j-1])*(v[i+1,j-1]+v[i,j-1]))/h_y)+\
+                0.5*(mu[i+1,j] + mu[i,j])/(0.5*(rho[i+1,j]+rho[i,j]))*\
+                ((u[i+1,j]-2*u[i,j]+u[i-1,j])/h_x**2+\
+                (u[i,j+1]-2*u[i,j]+u[i,j-1])/h_y**2 )+gx)
+                #To find the u velocity component at the right end of the pipe, use the discretized form of the continuity equation
+
+    #V STAR
+    for i in range(1,nx+1):
+        for j in range(1,ny):
+            vstar[i,j]=v[i,j]+dt*(-0.25*(((u[i,j+1]+u[i,j])*(v[i+1,j]+\
+                v[i,j])-(u[i-1,j+1]+u[i-1,j])*(v[i,j]+v[i-1,j]))/h_x+\
+                ((v[i,j+1]+v[i,j])**2-(v[i,j]+v[i,j-1])**2)/h_y)+\
+                0.5*(mu[i,j+1] + mu[i,j])/(0.5*(rho[i,j+1]+rho[i,j]))*\
+                ((v[i+1,j]-2*v[i,j]+v[i-1,j])/h_x**2+\
+                (v[i,j+1]-2*v[i,j]+v[i,j-1])/h_y**2 )+gy)
+
+    rt=rho.copy()
+    rt[:,0]=rho_w
+    rt[:,-1]=rho_w
+
+    for i in range(1,nx+1):
+        for j in range(1,ny+1):
+            p1[i,j]= (0.5/dt)*( (ustar[i,j]-ustar[i-1,j])/dx+(vt[i,j]-vt[i,j-1])/dy)
+            p2[i,j]=1.0/( (1./dx)*( 1./(dx*(rt[i+1,j]+rt[i,j]))+\
+                1./(dx*(rt[i-1,j]+rt[i,j])))+\
+                (1./dy)*(1./(dy*(rt[i,j+1]+rt[i,j]))+\
+                1./(dy*(rt[i,j-1]+rt[i,j]))))
