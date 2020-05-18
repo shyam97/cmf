d3cc110a806200b8d085d4b100ef2298f309ef98
Shyam Sundar Hemamalini <shyam.hemamalini@gmail.com>
2020-05-18 11:31:40 +0200

    New day, new hope

--- fluid side/boundary.py
+++ fluid side/boundary.py
@@ -1,2 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+vgrid[:,0] = 0
--- fluid side/solver.py
+++ fluid side/solver.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mesh import *
 
 l = 0.2
@@ -13,12 +12,15 @@ rho_s = 1.25
 p_a = 101325
 nu_s = 1.48e-5
 
-dt = 1e-4
+dt = 1e-2
+tsteps = 1e3
 
 grid,pgrid,ugrid,vgrid = create_mesh(l,w,h_x,h_y,md)
 
 p = matrix(pgrid) + p_a
 u = matrix(ugrid)
+ustar = matrix(ugrid)
+vstar = matrix(vgrid)
 v = matrix(vgrid)
 rho = matrix(pgrid) + rho_s
 nu = matrix(pgrid) + nu_s
@@ -31,6 +33,7 @@ p[:,-1] = 0
 rho[:,0] = 1e10
 rho[:,-1] = 1e10
 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 plt.figure()
 ax = plt.gca()
 im = ax.imshow(u.T, origin = 'lower', extent=[-h_x,l+h_x,-h_y,w+h_y])
@@ -38,3 +41,10 @@ divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(im, cax=cax)
 plt.show()
+
+for t in range(1,tsteps):
+    time = time + dt
+
+    u[:,0] = -u[:,1]
+    u[:,-1] = -u[:,-2]
+    p[-1,:] = 2*p_a - p[-2,:]
